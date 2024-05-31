d015cb4494bc52586e905f59ad716337c8147ef0
y1y2z3 <1152739029@qq.com>
2024-06-01 01:02:04 +0800

    modify: second person thread op

--- src/main/java/com/yyz/Student.java
+++ src/main/java/com/yyz/Student.java
@@ -12,6 +12,7 @@ public class Student {
         System.out.println("before reading!!!");
         System.out.println("second person add one operation before...");
         System.out.println("first person second add one operation before...");
+        System.out.println("second person add third operation before...");
         System.out.println(getName() + "is reading " + bookName + "book!");
         System.out.println("first person add one operation after...");
         System.out.println("second person second add one operation after...");
